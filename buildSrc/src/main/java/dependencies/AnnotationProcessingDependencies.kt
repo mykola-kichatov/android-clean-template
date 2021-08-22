6eaf6a7624ce300f7a93be7c4a4abaf92269f813
mkchtv <mykola.kichatov@gmail.com>
2021-08-22 15:05:54 +0300

    UPD: put Hilt compiler to annotation processor dependencies

--- buildSrc/src/main/java/dependencies/AnnotationProcessingDependencies.kt
+++ buildSrc/src/main/java/dependencies/AnnotationProcessingDependencies.kt
@@ -1,4 +1,9 @@
 package dependencies
 
+import constants.Versions
+
 object AnnotationProcessingDependencies {
+
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+
 }

10b47080a0307eef4b01181b3a6e3662e21ba16e
mkchtv <mykola.kichatov@gmail.com>
2021-08-23 08:40:39 +0300

    ADD: navigate to details by fab click

--- app/src/main/java/com/mkchtv/cleantemplate/list/ElementsListFragment.kt
+++ app/src/main/java/com/mkchtv/cleantemplate/list/ElementsListFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.mkchtv.cleantemplate.base.BaseFragment
 import com.mkchtv.cleantemplate.databinding.FragmentListBinding
+import com.mkchtv.cleantemplate.domain.common.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,7 +18,20 @@ class ElementsListFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View {
         val binding = FragmentListBinding.inflate(inflater, group, false)
+
+        setupFab(binding)
+
         return binding.root
+    }
+
+    private fun setupFab(binding: FragmentListBinding) =
+        binding.fab.setOnClickListener {
+            navigateToDetails(Constants.NEW_ELEMENT_ID)
+        }
+
+    private fun navigateToDetails(elementId: Int) {
+        val action = ElementsListFragmentDirections.actionListFragmentToDetailsFragment(elementId)
+        findNavController(this).navigate(action)
     }
 
 }
--- domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
+++ domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
@@ -2,6 +2,7 @@ package com.mkchtv.cleantemplate.domain.common
 
 object Constants {
 
+    const val NEW_ELEMENT_ID = -1
     const val KEY_INFO_MESSAGE_ELEMENT_DELETED = "info_element_deleted"
 
 }

da170f11c3654a75d311489b1388cd44a0a55bc6
mkchtv <mykola.kichatov@gmail.com>
2021-09-02 14:55:28 +0300

    FIX: do not use details viewmodel as viewmodel for now to fix build

--- app/src/main/java/com/mkchtv/cleantemplate/details/ElementDetailsViewModel.kt
+++ app/src/main/java/com/mkchtv/cleantemplate/details/ElementDetailsViewModel.kt
@@ -1,4 +1,12 @@
 package com.mkchtv.cleantemplate.details
 
-class ElementDetailsViewModel {
+import androidx.lifecycle.SavedStateHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+//@HiltViewModel
+class ElementDetailsViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+) {
+
 }

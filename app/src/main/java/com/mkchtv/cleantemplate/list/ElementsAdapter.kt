5dc581a3e9420da26febb661a4a745609802ad21
mkchtv <mykola.kichatov@gmail.com>
2021-09-02 14:57:39 +0300

    UPD: elements adapter refactoring

--- app/src/main/java/com/mkchtv/cleantemplate/list/ElementsAdapter.kt
+++ app/src/main/java/com/mkchtv/cleantemplate/list/ElementsAdapter.kt
@@ -2,25 +2,30 @@ package com.mkchtv.cleantemplate.list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.mkchtv.cleantemplate.databinding.ListItemElementBinding
 
 class ElementsAdapter(
-    private val items: List<ElementItem>
-) : RecyclerView.Adapter<ElementsAdapter.ViewHolder>() {
+    private val onItemClicked: (ElementItem) -> Unit
+) : ListAdapter<ElementItem, ElementsAdapter.ElementViewHolder>(DiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElementViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemElementBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
+        val viewHolder = ElementViewHolder(binding)
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onItemClicked(getItem(position))
+        }
+        return viewHolder
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
-        holder.bind(items[position])
+    override fun onBindViewHolder(holder: ElementViewHolder, position: Int) =
+        holder.bind(getItem(position))
 
-    override fun getItemCount(): Int = items.size
-
-    class ViewHolder(
+    class ElementViewHolder(
         private val binding: ListItemElementBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
@@ -29,6 +34,18 @@ class ElementsAdapter(
             binding.description.text = item.description
         }
 
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<ElementItem>() {
+            override fun areItemsTheSame(oldItem: ElementItem, newItem: ElementItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ElementItem, newItem: ElementItem): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 
 }

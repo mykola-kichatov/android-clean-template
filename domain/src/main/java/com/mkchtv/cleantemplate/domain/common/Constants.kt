5fed9e83429004a6e095f17873e26b707ce93dce
mkchtv <mykola.kichatov@gmail.com>
2021-09-06 17:39:44 +0300

    ADD: implementation of l10n getString(); ADD new string resources

--- domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
+++ domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
@@ -3,6 +3,9 @@ package com.mkchtv.cleantemplate.domain.common
 object Constants {
 
     const val NEW_ELEMENT_ID = -1
+
     const val KEY_INFO_MESSAGE_ELEMENT_DELETED = "info_element_deleted"
+    const val KEY_INFO_MESSAGE_ELEMENT_CREATED = "info_element_created"
+    const val KEY_INFO_MESSAGE_ELEMENT_UPDATED = "info_element_updated"
 
 }

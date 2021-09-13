070bfd41a7442d9fb1b3aecb928706f4e02b43a2
mkchtv <mykola.kichatov@gmail.com>
2021-09-13 17:13:26 +0300

    ADD: message notifier implementation

--- app/src/main/java/com/mkchtv/cleantemplate/common/DefaultUIMessageNotifier.kt
+++ app/src/main/java/com/mkchtv/cleantemplate/common/DefaultUIMessageNotifier.kt
@@ -1,20 +1,23 @@
 package com.mkchtv.cleantemplate.common
 
+import android.content.Context
+import android.widget.Toast
 import com.mkchtv.cleantemplate.domain.common.UIMessageNotifier
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DefaultUIMessageNotifier @Inject constructor(
-
+    @ApplicationContext private val context: Context
 ) : UIMessageNotifier {
 
     override fun notifyInfo(info: String) {
-        TODO("Not yet implemented")
+        Toast.makeText(context, info, Toast.LENGTH_SHORT).show()
     }
 
     override fun notifyError(error: String) {
-        TODO("Not yet implemented")
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
     }
 
 }

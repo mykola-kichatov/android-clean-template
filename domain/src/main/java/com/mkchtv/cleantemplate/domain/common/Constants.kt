33630e268990ee9e324920a95f647764c8084411
mkchtv <mykola.kichatov@gmail.com>
2021-09-13 17:11:40 +0300

    UPD: 0 is default id for new elements

--- domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
+++ domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
@@ -2,7 +2,7 @@ package com.mkchtv.cleantemplate.domain.common
 
 object Constants {
 
-    const val NEW_ELEMENT_ID = -1
+    const val NEW_ELEMENT_ID = 0
 
     const val KEY_INFO_MESSAGE_ELEMENT_DELETED = "info_element_deleted"
     const val KEY_INFO_MESSAGE_ELEMENT_CREATED = "info_element_created"

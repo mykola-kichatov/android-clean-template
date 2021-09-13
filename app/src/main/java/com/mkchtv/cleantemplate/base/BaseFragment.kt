34716d62c1ab9c1116b8f27aa3f09997a62989fb
mkchtv <mykola.kichatov@gmail.com>
2021-09-13 17:11:02 +0300

    ADD: fragment doesn't have menu by default

--- app/src/main/java/com/mkchtv/cleantemplate/base/BaseFragment.kt
+++ app/src/main/java/com/mkchtv/cleantemplate/base/BaseFragment.kt
@@ -1,8 +1,12 @@
 package com.mkchtv.cleantemplate.base
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(false)
+    }
 }

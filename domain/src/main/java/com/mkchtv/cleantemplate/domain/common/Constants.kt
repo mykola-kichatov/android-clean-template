35a954e0c277ab5164bb330a303647487f230498
mkchtv <mykola.kichatov@gmail.com>
2021-10-30 19:33:45 +0300

    UPD: remove unused constants

--- domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
+++ domain/src/main/java/com/mkchtv/cleantemplate/domain/common/Constants.kt
@@ -4,10 +4,6 @@ object Constants {
 
     const val NEW_ELEMENT_ID = 0
 
-    const val KEY_INFO_MESSAGE_ELEMENT_DELETED = "info_element_deleted"
-    const val KEY_INFO_MESSAGE_ELEMENT_CREATED = "info_element_created"
-    const val KEY_INFO_MESSAGE_ELEMENT_UPDATED = "info_element_updated"
-
     const val ARG_KEY_ELEMENT_ID = "id"
 
     const val TRANSITION_NAME_ELEMENT_ITEM = "element_item_transition"

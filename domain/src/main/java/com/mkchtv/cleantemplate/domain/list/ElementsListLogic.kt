9b085ba20d7a2fd48fb0bfb65f39c211e97105a1
mkchtv <mykola.kichatov@gmail.com>
2021-10-30 19:12:41 +0300

    ADD: use app-level scope to delete element on list screen

--- domain/src/main/java/com/mkchtv/cleantemplate/domain/list/ElementsListLogic.kt
+++ domain/src/main/java/com/mkchtv/cleantemplate/domain/list/ElementsListLogic.kt
@@ -5,8 +5,10 @@ import com.mkchtv.cleantemplate.domain.base.UILogic
 import com.mkchtv.cleantemplate.domain.common.Constants
 import com.mkchtv.cleantemplate.domain.common.L10N
 import com.mkchtv.cleantemplate.domain.common.UIMessageNotifier
+import com.mkchtv.cleantemplate.domain.di.AppIoScope
 import com.mkchtv.cleantemplate.domain.entity.Element
 import com.mkchtv.cleantemplate.domain.repository.ElementsRepository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,17 +24,20 @@ interface ElementsListLogic : UILogic {
 class DefaultElementsListLogic @Inject constructor(
     private val repository: ElementsRepository,
     private val uiMessageNotifier: UIMessageNotifier,
-    private val l10N: L10N
+    private val l10N: L10N,
+    @AppIoScope private val appIoScope: CoroutineScope
 ) : BaseUILogic(), ElementsListLogic {
 
     override fun elementsFlow(): Flow<List<Element>> =
         repository.elementsFlow()
 
     override fun onDeleteConfirmed(element: Element) {
-        uiScope.launch {
+        appIoScope.launch {
             repository.delete(element)
-            val info = l10N.getString(Constants.KEY_INFO_MESSAGE_ELEMENT_DELETED)
-            uiMessageNotifier.notifyInfo(info)
+            uiScope.launch {
+                val info = l10N.getString(Constants.KEY_INFO_MESSAGE_ELEMENT_DELETED)
+                uiMessageNotifier.notifyInfo(info)
+            }
         }
     }
 }

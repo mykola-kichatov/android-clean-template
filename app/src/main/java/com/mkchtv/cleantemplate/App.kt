386643d5d5e851a1039695789253252423a1ace3
mkchtv <mykola.kichatov@gmail.com>
2024-07-25 12:21:43 +0300

    UPD: no need to use multidex lib (min api is 21)

--- app/src/main/java/com/mkchtv/cleantemplate/App.kt
+++ app/src/main/java/com/mkchtv/cleantemplate/App.kt
@@ -1,7 +1,7 @@
 package com.mkchtv.cleantemplate
 
-import androidx.multidex.MultiDexApplication
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : MultiDexApplication()
+class App : Application()
